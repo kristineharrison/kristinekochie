2ced51a0f80099a6ca8c69cb0f24baacce3f4a2e
kkochie <kristinekochie@gmail.com>
2023-01-08 15:33:53 -0500

    Update project post links

--- data/projectsData.js
+++ data/projectsData.js
@@ -3,25 +3,25 @@ const projectsData = [
     title: 'WORKSPACE',
     description: `Productivity app to help manage image assets related to design projects.`,
     imgSrc: '/static/images/projects/previews/workspace-preview.jpg',
-    href: '/blog/workspace',
+    href: '/blog/projects/workspace',
   },
   {
     title: 'Cookbook Club',
     description: `Site for a local group to showcase club meetings and book choices.`,
     imgSrc: '/static/images/projects/previews/cookbook-club-preview.jpg',
-    href: '/blog/cookbook-club',
+    href: '/blog/projects/cookbook-club',
   },
-  {
-    title: 'CSS Color Tool',
-    description: `Flatiron School Phase 1 Project.`,
-    imgSrc: '/static/images/projects/previews/.jpg',
-    href: '/blog/css-color-tool',
-  },
+  // {
+  //   title: 'CSS Color Tool',
+  //   description: `Flatiron School Phase 1 Project.`,
+  //   imgSrc: '/static/images/projects/previews/.jpg',
+  //   href: '/blog/projects/css-color-tool',
+  // },
   {
     title: 'Grey Printing',
     description: `E-commerce site and rebranding for Grey Printing.`,
     imgSrc: '/static/images/projects/previews/grey-printing-preview.jpg',
-    href: '/blog/grey-printing',
+    href: '/blog/projects/grey-printing',
   },
 ]
 
