6a49275b24b21d5531ee86f0f1e4575a9b63a213
Kristine Kochie <kristinekochie@gmail.com>
2023-02-01 21:15:18 -0500

    Update dark mode text color

--- components/Intro.js
+++ components/Intro.js
@@ -4,13 +4,11 @@ export default function Intro() {
   return (
     <>
       <div className="justify-cente mt-20 flex flex-col items-center">
-        <p className="dark:text-gray-000 prose mt-20 max-w-none text-2xl text-gray-900">
-          Hello, I&apos;m
-        </p>
+        <p className="mt-20 text-2xl dark:text-gray-300">Hello, I&apos;m</p>
         <h1 className="font-serif text-3xl leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
           Kristine Kochie
         </h1>
-        <p className="mt-1 divide-gray-200 text-2xl dark:divide-gray-700">
+        <p className="mt-1 text-2xl dark:text-gray-300">
           <ReactTypingEffect staticText="I am a " text={['developer', 'designer', 'maker']} />
         </p>
       </div>
