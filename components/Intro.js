85122fc7f8f082a68c667dace0db2b22951e2ddc
Kristine Kochie <kristinekochie@gmail.com>
2023-02-01 22:53:14 -0500

    Add computer image to Intro component

--- components/Intro.js
+++ components/Intro.js
@@ -3,14 +3,23 @@ import ReactTypingEffect from 'react-typing-effect'
 export default function Intro() {
   return (
     <>
-      <div className="justify-cente mt-20 flex flex-col items-center">
+      <div className="mt-20 flex flex-col items-center justify-center">
         <p className="mt-20 text-2xl dark:text-gray-300">Hello, I&apos;m</p>
         <h1 className="font-serif text-3xl leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
           Kristine Kochie
         </h1>
         <p className="mt-1 text-2xl dark:text-gray-300">
-          <ReactTypingEffect staticText="I am a " text={['developer', 'designer', 'maker']} />
+          <ReactTypingEffect
+            staticText="I am a "
+            text={['developer.', 'coder.', 'designer.', 'maker.']}
+          />
         </p>
+      </div>
+      <div className="mt-20 flex justify-end">
+        <img
+          src="/static/images/old-computer-amber-flipped.png"
+          className="w-200 h-177 object-scale-down"
+        />
       </div>
     </>
   )
