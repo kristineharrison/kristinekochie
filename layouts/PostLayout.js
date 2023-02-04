f07819a2efd5c4a0e5d636d00f0f21447e8095a2
Kristine Kochie <kristinekochie@gmail.com>
2023-02-04 12:41:03 -0500

    Update Post header with social links

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -7,6 +7,7 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import Comments from '@/components/comments'
 import ScrollTopAndComment from '@/components/ScrollTopAndComment'
+import SocialIcon from '@/components/social-icons'
 
 const editUrl = (fileName) => `${siteMetadata.siteRepo}/blob/master/data/blog/${fileName}`
 const discussUrl = (slug) =>
@@ -50,7 +51,41 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
             className="divide-y divide-gray-200 pb-8 dark:divide-gray-300 xl:grid xl:grid-cols-4 xl:gap-x-6 xl:divide-y-0"
             style={{ gridTemplateRows: 'auto 1fr' }}
           >
-            <dl className="pt-6 pb-10 xl:border-b xl:border-gray-200 xl:pt-11 xl:dark:border-gray-700">
+            <div className="pt-6 pb-10 xl:border-b xl:border-gray-200 xl:pt-11 xl:dark:border-gray-700">
+              <div className="sr-only">Authors</div>
+              <ul className="flex justify-center space-x-8 sm:space-x-12 xl:block xl:space-x-0 xl:space-y-8">
+                {authorDetails.map((author) => (
+                  <li className="flex items-center space-x-2" key={author.name}>
+                    {author.avatar && (
+                      <Image
+                        src={author.avatar}
+                        width="38px"
+                        height="38px"
+                        alt="avatar"
+                        className="h-10 w-10 rounded-full"
+                      />
+                    )}
+                    <dl className="whitespace-nowrap text-sm font-medium leading-5">
+                      <dt className="sr-only">Name</dt>
+                      <dd className="font-serif text-gray-900 dark:text-gray-100">{author.name}</dd>
+                    </dl>
+                  </li>
+                ))}
+
+                <div className="mb-1 flex items-center space-x-4">
+                  <li>
+                    <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size="6" />
+                  </li>
+                  <li>
+                    <SocialIcon kind="github" href={siteMetadata.github} size="6" />
+                  </li>
+                  <li>
+                    <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="6" />
+                  </li>
+                </div>
+              </ul>
+            </div>
+            {/* <dl className="pt-6 pb-10 xl:border-b xl:border-gray-200 xl:pt-11 xl:dark:border-gray-700">
               <dt className="sr-only">Authors</dt>
               <dd>
                 <ul className="flex justify-center space-x-8 sm:space-x-12 xl:block xl:space-x-0 xl:space-y-8">
@@ -73,9 +108,23 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
                       </dl>
                     </li>
                   ))}
+
+                  <div className="mt-16 flex items-center">
+                    <div className="mb-3 flex space-x-4">
+                      <li>
+                        <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size="6" />
+                      </li>
+                      <li>
+                        <SocialIcon kind="github" href={siteMetadata.github} size="6" />
+                      </li>
+                      <li>
+                        <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="6" />
+                      </li>
+                    </div>
+                  </div>
                 </ul>
               </dd>
-            </dl>
+            </dl> */}
             <div className="divide-y divide-gray-200 dark:divide-gray-300 xl:col-span-3 xl:row-span-2 xl:pb-0">
               <div className="prose max-w-none pt-10 pb-8 dark:prose-dark">{children}</div>
               {/* <div className="pt-6 pb-6 text-sm text-gray-700 dark:text-gray-300">
